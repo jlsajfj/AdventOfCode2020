8a5e2e9192597d55e50f90b310e9b41158301d80
jlsajfj <jlsajfj@hotmail.com>
2020-12-09 16:41:45 -0500

    day 1 complete

--- day1.py
+++ day1.py
@@ -1,8 +1,8 @@
-er = open('day1.in','r').read().split('\n') #er meaning expense report
+er = list(map(int,open('day1.in','r').read().strip().split('\n'))) #er meaning expense report
 
-for i in range(len(er)):
- for j in range(i+1,len(er)):
-  print(er[i]+er[j])
+for i in range(len(er)): #first number loop
+ for j in range(i+1,len(er)): #second number loop
+  #print(er[i]+er[j]) #just for debugging
   if er[i]+er[j]==2020:
    print(er[i]*er[j])
    exit()
