824a07b68522c79e47a73617cae69e9f29a70982
jlsajfj <jlsajfj@hotmail.com>
2020-12-09 23:00:04 -0500

    finished one liners and comments for first, adding explanation for second tomorrow

--- day2-1.py
+++ day2-1.py
@@ -14,6 +14,47 @@ print(num)
 # technically this can be done on one line
 
 # input:
-# list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))
+# map((lambda x: [x[0].split(' '),x[1].strip()]),map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))
 
-print(len(list(filter(lambda ps: ((lambda x: ((lambda m: m[0] <= x and x <= m[1])(list(map(int,ps[0][0].split('-'))))))(ps[1].count(ps[0][1]))), list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))))))
+# processing:
+# lambda ps: (lambda x: (lambda m: m[0] <= x and x <= m[1])(list(map(int,ps[0][0].split('-')))))(ps[1].count(ps[0][1]))
+
+print(len(list(filter(lambda ps: (lambda x: (lambda m: m[0] <= x and x <= m[1])(list(map(int,ps[0][0].split('-')))))(ps[1].count(ps[0][1])), map((lambda x: [x[0].split(' '),x[1].strip()]),map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))))
+
+"""
+ explanation:
+ 
+ print(len(list(filter(...))))
+  this just uses a filter to remove stuff that
+  doesnt fall under constraints, sets it to a
+  list, and outputs the size of the list.
+ 
+ lambda ps: ...
+  this just sets the current working element to ps
+ 
+ (lambda x: ...)(ps[1].count(ps[0][1]))
+  set the count variable to x
+ 
+ (lambda m: ...)(list(map(int,ps[0][0].split('-'))))
+  sets the array of min,max to the variable map
+  the list is necessary for the lambda to function
+ 
+ m[0] <= x and x <= m[1]
+  condition for filter to keep element
+ 
+ open('day2.in','r').read().strip().split('\n')
+  open and read file, removing excess new lines,
+  then splitting by new line
+ 
+ map(lambda x: x.split(':'),...)
+  split each element by the colon. originally this
+  had list(...) wrapping it, but that turned out
+  to be unnecessary.
+ 
+ map((lambda x: [x[0].split(' '),x[1].strip()]),...)
+  this takes each value, then further splits the
+  first section (the "1-3" section) and strips
+  the second section of unnecessary spaces. this
+  is unnecessary, i could have just used x[1][1:]
+  instead. it also puts it into a neat array
+"""
--- day2-2.py
+++ day2-2.py
@@ -11,3 +11,20 @@ for ps in pw: #ps meaning password (requirement) set
  if (ff and not ss) or (ss and not ff): #update
   num += 1
 print(num)
+
+# this too can be one lined!
+
+# input (same as before):
+# list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))
+
+# processing:
+# lambda ps: (lambda p: (lambda x: ((x[0] and not x[1]) or (x[1] and not x[0])))([ps[1][p[0]-1]==ps[0][1],ps[1][p[1]-1]==ps[0][1]]))(list(map(int,ps[0][0].split('-'))))
+# debug:
+# lambda ps: (lambda p: (lambda x: (print("{} {} {} {} {}".format(x[0],x[1],ps[0][1],ps[1][p[0]-1],ps[1][p[1]-1]))))([ps[1][p[0]-1]==ps[0][1],ps[1][p[1]-1]==ps[0][1]]))(list(map(int,ps[0][0].split('-'))))
+
+print(len(list(filter(lambda ps: (lambda p: (lambda x: ((x[0] and not x[1]) or (x[1] and not x[0])))([ps[1][p[0]-1]==ps[0][1],ps[1][p[1]-1]==ps[0][1]]))(list(map(int,ps[0][0].split('-')))),list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))))))
+
+"""
+explanation:
+ i'll do this tomorrow
+"""
