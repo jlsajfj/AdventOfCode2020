290616856dbbade9dd323051103e38d35698e452
jlsajfj <jlsajfj@hotmail.com>
2020-12-09 19:03:30 -0500

    added one liner for day 2

--- day2-1.py
+++ day2-1.py
@@ -10,3 +10,10 @@ for ps in pw: #ps meaning password (requirement) set
  if min <= cnt and cnt <= max: #update
   num += 1
 print(num)
+
+# technically this can be done on one line
+
+# input:
+# list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))
+
+print(len(list(filter(lambda ps: ((lambda x: ((lambda m: m[0] <= x and x <= m[1])(list(map(int,ps[0][0].split('-'))))))(ps[1].count(ps[0][1]))), list(map((lambda x: [x[0].split(' '),x[1].strip()]),list(map(lambda x: x.split(':'),open('day2.in','r').read().strip().split('\n')))))))))
