708c44bc3708f2d3460796ae56183e8585259068
jlsajfj <jlsajfj@hotmail.com>
2020-12-14 21:31:04 -0500

    i can't be bothered today

--- day5-1.py
+++ day5-1.py
@@ -1,1 +1,11 @@
-seats = open('day5.in','r').read().split('\n')
+seats = open('day5.in','r').read().strip().split('\n')
+max = 0
+
+for seat in seats:
+ l,h = 0, 1023
+ for p in seat:
+  if p == 'F':
+   h = int((l+h)/2)
+  else:
+   l = int((l+h)/2.0+0.5)
+  print(l)
